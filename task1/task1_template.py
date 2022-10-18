67da93c9a9686ab723cbce566ba9ccf53239ef64
Xingyun(Saxon) SA <s2305139@sms.ed.ac.uk>
2022-10-18 17:21:27 +0100

    add task1

--- task1/task1_template.py
+++ task1/task1_template.py
@@ -65,24 +65,31 @@ endEffector = "LARM_JOINT5"
 targetPosition = np.array([0.37, 0.23, 1.06385])  # x,y,z coordinates in world frame
 
 # Example code. Feel free to modify
-pltTime, pltEFPosition = sim.move_without_PD(endEffector, targetPosition, speed=0.01, orientation=None, threshold=1e-3, maxIter=3000, debug=False, verbose=False)
+
+# pltTime, pltEFPosition ---
+sim.move_without_PD(endEffector, targetPosition, speed=0.01, orientation=None, threshold=1e-3, maxIter=3000, debug=False, verbose=False)
 
 
 # Now plot some graphs
-task1_figure_name = "task1_kinematics.png"
-task1_savefig = True
+# task1_figure_name = "task1_kinematics.png"
+# task1_savefig = True
 # ...
 
-fig = plt.figure(figsize=(6, 4))
+# fig = plt.figure(figsize=(6, 4))
 
-plt.plot(pltTime, pltEFPosition, color='blue')
-plt.xlabel("Time s")
-plt.ylabel("Distance to target position")
+# plt.plot(pltTime, pltEFPosition, color='blue')
+# plt.xlabel("Time s")
+# plt.ylabel("Distance to target position")
 
-plt.suptitle("task1 IK without PD", size=16)
-plt.tight_layout()
-plt.subplots_adjust(left=0.15)
+# plt.suptitle("task1 IK without PD", size=16)
+# plt.tight_layout()
+# plt.subplots_adjust(left=0.15)
 
-if task1_savefig:
-    fig.savefig(task1_figure_name)
-plt.show()
+# if task1_savefig:
+#     fig.savefig(task1_figure_name)
+# plt.show()
+
+try:
+    time.sleep(float(sys.argv[1]))
+except:
+    time.sleep(10)
